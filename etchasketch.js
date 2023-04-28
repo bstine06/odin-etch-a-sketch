09680cdf31689fe758408f61d0706d3fd7c28b36
Brett Stine <r.brettstine@gmail.com>
2023-04-27 23:11:29 -0400

    Add mouseover functionality to "color" grid pixels
     - also added various styling improvements

--- etchasketch.js
+++ etchasketch.js
@@ -3,5 +3,10 @@ const gridContainer = document.querySelector('#gridContainer');
 for (let i = 0; i < 256; i++) {
     const newPixel = document.createElement('div');
     newPixel.classList.add('pixel');
+    newPixel.addEventListener('mouseover', draw);
     gridContainer.appendChild(newPixel);
+}
+
+function draw() {
+    this.classList.add('drawn');
 }
